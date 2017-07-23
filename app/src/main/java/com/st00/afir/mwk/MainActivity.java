4492cbeca1685ae24fdda8756ef4191390ef8a7c
zakafir <zak.afir@gmail.com>
2017-07-23 21:08:55 +0200

    Adding new Activities

--- app/src/main/java/com/st00/afir/mwk/MainActivity.java
+++ app/src/main/java/com/st00/afir/mwk/MainActivity.java
@@ -1,13 +1,56 @@
 package com.st00.afir.mwk;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tvFamily,tvPhrases,tvColors,tvNumbers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvNumbers = (TextView)findViewById(R.id.numbers);
+        tvColors = (TextView)findViewById(R.id.colors);
+        tvPhrases = (TextView)findViewById(R.id.phrases);
+        tvFamily = (TextView)findViewById(R.id.family_members);
+
+        //button events
+        tvNumbers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(MainActivity.this, NumbersActivity.class);
+                MainActivity.this.startActivity(myIntent);
+            }
+        });
+
+        tvColors.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(MainActivity.this, ColorsActivity.class);
+                MainActivity.this.startActivity(myIntent);
+            }
+        });
+
+        tvPhrases.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(MainActivity.this, PhrasesActivity.class);
+                MainActivity.this.startActivity(myIntent);
+            }
+        });
+
+        tvFamily.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(MainActivity.this, FamilyActivity.class);
+                MainActivity.this.startActivity(myIntent);
+            }
+        });
     }
 }
